763f340198208a37b2208d05e0795e0fb6ff93e2
Manish Sharma <sharmamanish8301@gmail.com>
2022-03-12 18:28:43 +0530

    button bg change

--- components/button.js
+++ components/button.js
@@ -9,8 +9,8 @@ const Button = ({ id, selectedButton, setSelectedButton }) => {
 				dispatch(fetchUser(id));
 				setSelectedButton(id);
 			}}
-			className={`bg-gradient-to-r from-cyan-500 to-blue-500 py-3 shadow-lg shadow-blue-500/40 px-2 rounded-xl transition-all duration-500 ${
-				id === selectedButton ? 'ring-4 ring-blue-500/50' : ''
+			className={`bg-gradient-to-tr from-gray-800 to-gray-900 py-3 shadow-lg px-2 rounded-xl transition-all duration-500 ${
+				id === selectedButton ? 'ring-4 ring-gray-500/50' : ''
 			}`}
 		>
 			{id}
